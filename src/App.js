7a264a8af2ae2b38dcf2f7ae2bf9c5559e014d38
Winnie1286 <83891347+Winnie1286@users.noreply.github.com>
2021-09-27 00:35:33 -0400

    added image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+import typewriter from "./typewriter.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <img src={typewriter} className="App-logo img-fluid" alt="logo" />
+      </header>
     </div>
   );
 }
